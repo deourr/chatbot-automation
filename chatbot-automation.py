97c526386b7b99817ba5e1d76807b0f41ef89684
rahul deou <rahuldeou@gmail.com>
2019-04-10 07:44:22 +0530

    Final version

--- chatbot-automation.py
+++ chatbot-automation.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# chatbot automation
 
 import nltk
 import warnings
@@ -6,7 +6,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import random
-import string  # to process standard python strings
+import string  
 
 chatbot_automation_file = open('chatbot-automation.txt', 'r', errors='ignore')
 chatbot_automation_file_raw = chatbot_automation_file.read()
@@ -16,20 +16,20 @@ word_tokens = nltk.word_tokenize(chatbot_automation_file_raw)  # converts to lis
 
 sent_tokens[:2]
 
-word_tokens[:5]
+word_tokens[:4]
 
-lemmer = nltk.stem.WordNetLemmatizer()
+wordNetLemmer = nltk.stem.WordNetLemmatizer()
 
 
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
+def lemmatizerToken(tokens):
+    return [wordNetLemmer.lemmatize(token) for token in tokens]
 
 
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 
 
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+def lemmatizerNormalize(text):
+    return lemmatizerToken(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 
 USER_REQUESTS = ("hello", "hi", "hey",)
@@ -51,7 +51,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 def response(user_response):
     bot_response = ''
     sent_tokens.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    TfidfVec = TfidfVectorizer(tokenizer=lemmatizerNormalize, stop_words='english')
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx = vals.argsort()[0][-2]
